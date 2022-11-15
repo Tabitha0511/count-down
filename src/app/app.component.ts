2ca5138c12348081a2dbb5563d11dea121458ba0
Tabitha0511 <tabitharavichandran0511@gmail.com>
2022-11-15 22:41:12 +0530

    app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  intervalFired(event) {
+    return event % 2 === 0
+      ? this.evenNumbers.push(event)
+      : this.oddNumbers.push(event);
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"],
 })
-export class GameControlComponent implements OnInit {
+export class GameControlComponent {
+  @Output() firedNumber: any = new EventEmitter();
+  lastInterval = 0;
+  interval: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  start() {
+    this.interval = setInterval(() => {
+      this.firedNumber.emit(this.lastInterval + 1);
+      this.lastInterval++;
+    }, 1000);
   }
 
+  stop() {
+    clearInterval(this.interval);
+  }
 }
